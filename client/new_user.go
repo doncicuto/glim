fdfdca9099d465c8eba30f7d449aa00d6a76776b
Miguel A. C <doncicuto@gmail.com>
2020-09-26 19:31:27 +0200

    feat(cli): add user from cli

--- client/new_user.go
+++ client/new_user.go
@@ -23,9 +23,23 @@
 package client
 
 import (
+	"bufio"
+	"crypto/tls"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
+	"github.com/Songmu/prompter"
+	"github.com/badoux/checkmail"
+	resty "github.com/go-resty/resty/v2"
+	"github.com/muultipla/glim/models"
 	"github.com/spf13/cobra"
+)
+
+var (
+	fullname, email   string
+	manager, readonly bool
 )
 
 // NewUserCmd - TODO comment
@@ -33,6 +47,131 @@ var newUserCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create a Glim user account",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("")
+
+		url := "https://127.0.0.1:1323" // TODO - This should not be hardcoded
+
+		// Validate email
+		if err := checkmail.ValidateFormat(email); err != nil {
+			fmt.Println("email should have a valid format")
+			os.Exit(1)
+		}
+
+		// Check if both manager and readonly has been set
+		if manager && readonly {
+			fmt.Println("a Glim account cannot be both manager and readonly at the same time")
+			os.Exit(1)
+		}
+
+		// Prompt for password if needed
+		if !cmd.Flags().Changed("password") {
+			if !passwordStdin {
+				password = prompter.Password("Password")
+				if password == "" {
+					fmt.Println("Error password required")
+					os.Exit(1)
+				}
+				confirmPassword := prompter.Password("Confirm password")
+				if password != confirmPassword {
+					fmt.Println("Error passwords don't match")
+					os.Exit(1)
+				}
+			}
+		} else {
+			fmt.Println("WARNING! Using --password via the CLI is insecure. Use --password-stdin.")
+		}
+
+		// Check if password has been sent in stdin
+		if passwordStdin {
+			if password != "" {
+				fmt.Println("--password and --password-stdin are mutually exclusive")
+				os.Exit(1)
+			} else {
+				// Reference: https://flaviocopes.com/go-shell-pipes/
+				info, err := os.Stdin.Stat()
+				if err != nil {
+					fmt.Println("Error reading from stdin")
+					os.Exit(1)
+				}
+
+				if info.Mode()&os.ModeCharDevice != 0 {
+					fmt.Println("Error expecting password from stdin using a pipe")
+					os.Exit(1)
+				}
+
+				reader := bufio.NewReader(os.Stdin)
+				var output []rune
+
+				for {
+					input, _, err := reader.ReadRune()
+					if err != nil && err == io.EOF {
+						break
+					}
+					output = append(output, input)
+				}
+
+				password = strings.TrimSuffix(string(output), "\n")
+			}
+		}
+
+		// Glim server URL
+		if len(args) > 0 {
+			url = args[0]
+		}
+
+		// Read credentials
+		token := ReadCredentials()
+		endpoint := fmt.Sprintf("%s/users", url)
+		// Check expiration
+		if NeedsRefresh(token) {
+			Refresh(token.RefreshToken)
+			token = ReadCredentials()
+		}
+
+		// Rest API authentication
+		client := resty.New()
+		client.SetAuthToken(token.AccessToken)
+		// TODO - We should verify server's certificate
+		client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+		resp, err := client.R().
+			SetHeader("Content-Type", "application/json").
+			SetBody(models.User{
+				Username: &username,
+				Password: &password,
+				Fullname: &fullname,
+				Email:    &email,
+				Manager:  &manager,
+				Readonly: &readonly,
+			}).
+			SetError(&APIError{}).
+			Post(endpoint)
+
+		if err != nil {
+			fmt.Printf("Error connecting with Glim: %v\n", err)
+			os.Exit(1)
+		}
+
+		if resp.IsError() {
+			fmt.Printf("Error response from Glim: %v\n", resp.Error().(*APIError).Message)
+			os.Exit(1)
+		}
+
+		fmt.Println("User created")
 	},
+}
+
+func init() {
+	rootCmd.AddCommand(newUserCmd)
+
+	newUserCmd.Flags().StringVarP(&username, "username", "u", "", "Username")
+	newUserCmd.Flags().StringVarP(&fullname, "fullname", "f", "", "Fullname")
+	newUserCmd.Flags().StringVarP(&email, "email", "e", "", "Email")
+	newUserCmd.Flags().StringVarP(&password, "password", "p", "", "Password")
+	newUserCmd.Flags().BoolVar(&passwordStdin, "password-stdin", false, "Take the password from stdin")
+	newUserCmd.Flags().BoolVar(&manager, "manager", false, "Glim manager account?")
+	newUserCmd.Flags().BoolVar(&readonly, "readonly", false, "Glim readonly account?")
+
+	// Mark required flags
+	cobra.MarkFlagRequired(newUserCmd.Flags(), "username")
+	cobra.MarkFlagRequired(newUserCmd.Flags(), "fullname")
+	cobra.MarkFlagRequired(newUserCmd.Flags(), "email")
 }
