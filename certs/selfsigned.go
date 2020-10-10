c6b2b250464973d66fe0d005d5853683271d03fa
Miguel A. C <doncicuto@gmail.com>
2020-10-10 19:54:52 +0200

    feat(glim): add licence to files

--- certs/selfsigned.go
+++ certs/selfsigned.go
@@ -1,16 +1,18 @@
-// Copyright 2020 Miguel Ángel Álvarez Cabrerizo <mcabrerizo@arrakis.ovh>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+Copyright © 2020 Miguel Ángel Álvarez Cabrerizo <mcabrerizo@arrakis.ovh>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 // Copyright 2019 Meter, Inc.
 // Permission is hereby granted, free of charge, to any person obtaining a copy
