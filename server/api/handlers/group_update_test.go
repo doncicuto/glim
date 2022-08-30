4bc094ecd1269982d789196bd9075422957a2aaf
Miguel Cabrerizo <doncicuto@gmail.com>
2022-08-30 18:49:00 +0200

    feat: add REST tests for group_update

---
+++ server/api/handlers/group_update_test.go
@@ -0,0 +1,115 @@
+package handlers
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGroupUpdate(t *testing.T) {
+	// New SQLite test database
+	db, err := newTestDatabase()
+	if err != nil {
+		t.Fatalf("could not initialize db - %v", err)
+	}
+	defer removeDatabase()
+
+	// New BadgerDB test key-value storage
+	kv, err := newTestKV()
+	if err != nil {
+		t.Fatalf("could not initialize kv - %v", err)
+	}
+	defer removeKV()
+
+	settings := testSettings(db, kv)
+	e := EchoServer(settings)
+	h := &Handler{DB: db, KV: kv}
+
+	// Log in with admin, search and/or plain user and get tokens
+	adminToken, _ := getUserTokens("admin", h, e, settings)
+
+	// Test cases
+	testCases := []RestTestCase{
+		{
+			name:             "uid not found in token",
+			expResCode:       http.StatusNotAcceptable,
+			reqURL:           "/v1/groups/1",
+			reqMethod:        http.MethodPut,
+			secret:           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcGkuZ2xpbS5zZXJ2ZXIiLCJleHAiOjE5NzcyNDUzOTksImlhdCI6MTY2MTYyNjA3MSwiaXNzIjoiYXBpLmdsaW0uc2VydmVyIiwianRpIjoiZTdiZmYzMjQtMzJmOC00MTNlLTgyNmYtNzc5Mzk5NDBjOTZkIiwibWFuYWdlciI6dHJ1ZSwicmVhZG9ubHkiOmZhbHNlLCJzdWIiOiJhcGkuZ2xpbS5jbGllbnQifQ.SQ0P6zliTGQiAdTi2DjCDeht0n2FjYdPGV7JgOx0TRY",
+			expectedBodyJSON: `{"message":"wrong token or missing info in token claims"}`,
+		},
+		{
+			name:             "non-existent manager user can't update account info",
+			expResCode:       http.StatusForbidden,
+			reqURL:           "/v1/groups/1",
+			reqMethod:        http.MethodPut,
+			secret:           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhcGkuZ2xpbS5zZXJ2ZXIiLCJleHAiOjE5NzcyNDUzOTksImlhdCI6MTY2MTYyNjA3MSwiaXNzIjoiYXBpLmdsaW0uc2VydmVyIiwianRpIjoiZTdiZmYzMjQtMzJmOC00MTNlLTgyNmYtNzc5Mzk5NDBjOTZkIiwibWFuYWdlciI6dHJ1ZSwicmVhZG9ubHkiOmZhbHNlLCJzdWIiOiJhcGkuZ2xpbS5jbGllbnQiLCJ1aWQiOjEwMDB9.amq5OV7gU7HUrn5YA8sbs2cXMRFeYHTmXm6bhXJ9PDg",
+			expectedBodyJSON: `{"message":"wrong user attempting to update group"}`,
+		},
+		{
+			name:             "group not found",
+			expResCode:       http.StatusNotFound,
+			reqURL:           "/v1/groups/100",
+			reqMethod:        http.MethodPut,
+			secret:           adminToken,
+			expectedBodyJSON: `{"message":"group not found"}`,
+		},
+		{
+			name:             "group devel can be created without members",
+			expResCode:       http.StatusOK,
+			reqURL:           "/v1/groups",
+			reqMethod:        http.MethodPost,
+			secret:           adminToken,
+			reqBodyJSON:      `{"name": "devel", "description": "Developers"}`,
+			expectedBodyJSON: `{"gid":1,"name":"devel","description":"Developers"}`,
+		},
+		{
+			name:             "group can be created without members",
+			expResCode:       http.StatusOK,
+			reqURL:           "/v1/groups",
+			reqMethod:        http.MethodPost,
+			secret:           adminToken,
+			reqBodyJSON:      `{"name": "managers", "description": "Managers"}`,
+			expectedBodyJSON: `{"gid":2,"name":"managers","description":"Managers"}`,
+		},
+		{
+			name:             "group name can't be duplicated",
+			expResCode:       http.StatusNotAcceptable,
+			reqURL:           "/v1/groups/2",
+			reqMethod:        http.MethodPut,
+			secret:           adminToken,
+			reqBodyJSON:      `{"name": "devel"}`,
+			expectedBodyJSON: `{"message":"group name cannot be duplicated"}`,
+		},
+		{
+			name:             "can update description",
+			expResCode:       http.StatusOK,
+			reqURL:           "/v1/groups/1",
+			reqMethod:        http.MethodPut,
+			secret:           adminToken,
+			reqBodyJSON:      `{"description": "Devs"}`,
+			expectedBodyJSON: `{"gid":1,"name":"devel","description":"Devs"}`,
+		},
+		{
+			name:             "can replace all members",
+			expResCode:       http.StatusOK,
+			reqURL:           "/v1/groups/1",
+			reqMethod:        http.MethodPut,
+			secret:           adminToken,
+			reqBodyJSON:      `{"members": "saul", "replace": true}`,
+			expectedBodyJSON: `{"gid":1,"name":"devel","description":"Devs","members":[{"uid":3,"username":"saul","name":"","firstname":"","lastname":"","email":"","ssh_public_key":"","manager":false,"readonly":false,"locked":false}]}`,
+		},
+		{
+			name:             "can add a member",
+			expResCode:       http.StatusOK,
+			reqURL:           "/v1/groups/1",
+			reqMethod:        http.MethodPut,
+			secret:           adminToken,
+			reqBodyJSON:      `{"members": "kim"}`,
+			expectedBodyJSON: `{"gid":1,"name":"devel","description":"Devs","members":[{"uid":3,"username":"saul","name":"","firstname":"","lastname":"","email":"","ssh_public_key":"","manager":false,"readonly":false,"locked":false},{"uid":4,"username":"kim","name":"","firstname":"","lastname":"","email":"","ssh_public_key":"","manager":false,"readonly":false,"locked":false}]}`,
+		},
+	}
+
+	for _, tc := range testCases {
+		runTests(t, tc, e)
+	}
+}
