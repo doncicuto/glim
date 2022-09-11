1dfd865d8c937ea0da9bf634b63e3c93e9bc846a
Miguel Cabrerizo <doncicuto@gmail.com>
2022-09-11 09:05:30 +0200

    docs: replace email

--- cmd/login.go
+++ cmd/login.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 Miguel Ángel Álvarez Cabrerizo <mcabrerizo@arrakis.ovh>
+Copyright © 2022 Miguel Ángel Álvarez Cabrerizo <mcabrerizo@sologitops.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
