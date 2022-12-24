5fe1cfcd2eac9427040adbd402d713441653d078
Miguel Cabrerizo <doncicuto@gmail.com>
2022-12-24 12:34:20 +0100

    qa: remove dups in group_create and group_delete tests

--- cmd/group_create_test.go
+++ cmd/group_create_test.go
@@ -7,6 +7,12 @@ import (
 )
 
 func TestNewGroupCmd(t *testing.T) {
+	const endpoint = "http://127.0.0.1:51021"
+	const serverFlag = "--server"
+	const descriptionFlag = "--description"
+	const membersFlag = "--members"
+	const groupFlag = "--group"
+
 	dbPath := uuid.New()
 	e := testSetup(t, dbPath.String(), false)
 	defer testCleanUp(dbPath.String())
@@ -22,42 +28,42 @@ func TestNewGroupCmd(t *testing.T) {
 		{
 			name:           "login successful",
 			cmd:            LoginCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51021", "--username", "admin", "--password", "test"},
+			args:           []string{serverFlag, endpoint, "--username", "admin", "--password", "test"},
 			errorMessage:   "",
 			successMessage: "Login succeeded\n",
 		},
 		{
 			name:           "new group test",
 			cmd:            NewGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51021", "--group", "test", "--description", "test", "--members", "kim,saul"},
+			args:           []string{serverFlag, endpoint, groupFlag, "test", descriptionFlag, "test", membersFlag, "kim,saul"},
 			errorMessage:   "",
 			successMessage: "Group created\n",
 		},
 		{
 			name:           "group already exists",
 			cmd:            NewGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51021", "--group", "test", "--description", "test", "--members", "kim,saul"},
+			args:           []string{serverFlag, endpoint, groupFlag, "test", descriptionFlag, "test", membersFlag, "kim,saul"},
 			errorMessage:   "group already exists",
 			successMessage: "",
 		},
 		{
 			name:           "new group killers",
 			cmd:            NewGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51021", "--group", "killers", "--description", "test", "--members", "charles"},
+			args:           []string{serverFlag, endpoint, groupFlag, "killers", descriptionFlag, "test", membersFlag, "charles"},
 			errorMessage:   "",
 			successMessage: "Group created\n",
 		},
 		{
 			name:           "login successful as kim",
 			cmd:            LoginCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51021", "--username", "kim", "--password", "test"},
+			args:           []string{serverFlag, endpoint, "--username", "kim", "--password", "test"},
 			errorMessage:   "",
 			successMessage: "Login succeeded\n",
 		},
 		{
 			name:           "kim can't add new group",
 			cmd:            NewGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51021", "--group", "killers", "--description", "test", "--members", "charles"},
+			args:           []string{serverFlag, endpoint, groupFlag, "killers", descriptionFlag, "test", membersFlag, "charles"},
 			errorMessage:   "user has no proper permissions",
 			successMessage: "",
 		},
--- cmd/group_delete_test.go
+++ cmd/group_delete_test.go
@@ -7,6 +7,11 @@ import (
 )
 
 func TestDeleteCmd(t *testing.T) {
+	const endpoint = "http://127.0.0.1:51013"
+	const forceFlag = "--force"
+	const groupFlag = "--group"
+	const serverFlag = "--server"
+
 	dbPath := uuid.New()
 	e := testSetup(t, dbPath.String(), false)
 	defer testCleanUp(dbPath.String())
@@ -22,63 +27,63 @@ func TestDeleteCmd(t *testing.T) {
 		{
 			name:           "login successful",
 			cmd:            LoginCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--username", "admin", "--password", "test"},
+			args:           []string{serverFlag, endpoint, "--username", "admin", "--password", "test"},
 			errorMessage:   "",
 			successMessage: "Login succeeded\n",
 		},
 		{
 			name:           "new group test",
 			cmd:            NewGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--group", "test", "--description", "test", "--members", "kim,saul"},
+			args:           []string{serverFlag, endpoint, groupFlag, "test", "--description", "test", "--members", "kim,saul"},
 			errorMessage:   "",
 			successMessage: "Group created\n",
 		},
 		{
 			name:           "new group killers",
 			cmd:            NewGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--group", "killers", "--description", "test", "--members", "charles"},
+			args:           []string{serverFlag, endpoint, groupFlag, "killers", "--description", "test", "--members", "charles"},
 			errorMessage:   "",
 			successMessage: "Group created\n",
 		},
 		{
 			name:           "delete group killers",
 			cmd:            DeleteGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--group", "killers", "--force"},
+			args:           []string{serverFlag, endpoint, groupFlag, "killers", forceFlag},
 			errorMessage:   "",
 			successMessage: "Group deleted\n",
 		},
 		{
 			name:           "list groups without killers",
 			cmd:            ListGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--json"},
+			args:           []string{serverFlag, endpoint, "--json"},
 			errorMessage:   "",
 			successMessage: `[{"gid":1,"name":"test","description":"test","members":[{"uid":3,"username":"saul","name":"","firstname":"","lastname":"","email":"","ssh_public_key":"","jpeg_photo":"","manager":false,"readonly":false,"locked":false},{"uid":4,"username":"kim","name":"","firstname":"","lastname":"","email":"","ssh_public_key":"","jpeg_photo":"","manager":false,"readonly":false,"locked":false}],"guac_config_protocol":"","guac_config_parameters":""}]` + "\n",
 		},
 		{
 			name:           "try to delete non-existent group",
 			cmd:            DeleteGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--group", "killers", "--force"},
+			args:           []string{serverFlag, endpoint, groupFlag, "killers", forceFlag},
 			errorMessage:   "group not found",
 			successMessage: "",
 		},
 		{
 			name:           "try to delete non-existent group using gid",
 			cmd:            DeleteGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "-i", "10", "--force"},
+			args:           []string{serverFlag, endpoint, "-i", "10", forceFlag},
 			errorMessage:   "group not found",
 			successMessage: "",
 		},
 		{
 			name:           "login successful as kim",
 			cmd:            LoginCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "--username", "kim", "--password", "test"},
+			args:           []string{serverFlag, endpoint, "--username", "kim", "--password", "test"},
 			errorMessage:   "",
 			successMessage: "Login succeeded\n",
 		},
 		{
 			name:           "try to delete group without permissions",
 			cmd:            DeleteGroupCmd(),
-			args:           []string{"--server", "http://127.0.0.1:51013", "-i", "1", "--force"},
+			args:           []string{serverFlag, endpoint, "-i", "1", forceFlag},
 			errorMessage:   "user has no proper permissions",
 			successMessage: "",
 		},
